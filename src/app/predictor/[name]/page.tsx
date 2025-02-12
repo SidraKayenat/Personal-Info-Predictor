ecee4e5b0b89a34c3b323a17979365a8dcf78e17
SidraKayenat <sidrakayenatofficial@gmail.com>
2025-02-12 17:26:02 +0500

    vercel err2 sol

--- src/app/predictor/[name]/page.tsx
+++ src/app/predictor/[name]/page.tsx
@@ -1,43 +1,42 @@
-//CREATE 3 FUNCTIONS
+import { NextPage } from "next";
 
 const getPredictedAge = async (name: string) => {
   const res = await fetch(`https://api.agify.io/?name=${name}`);
   return res.json();
 };
+
 const getPredictedGender = async (name: string) => {
   const res = await fetch(`https://api.genderize.io/?name=${name}`);
   return res.json();
 };
+
 const getPredictedCountry = async (name: string) => {
   const res = await fetch(`https://api.nationalize.io/?name=${name}`);
   return res.json();
 };
 
-interface Params {
-  name: string; // Change this to match the expected structure
-}
-
-export default async function Page({ params }: { params: Params }) {
+// Use NextPage type for the component
+const Page: NextPage<{ params: { name: string } }> = async ({ params }) => {
   const ageData = getPredictedAge(params.name);
   const genderData = getPredictedGender(params.name);
   const countryData = getPredictedCountry(params.name);
 
-  //if we have multiple APIs in same component, destrcuture it using array
-  //more than one promise at once
   const [age, gender, country] = await Promise.all([
     ageData,
     genderData,
     countryData,
   ]);
+
   return (
     <div>
       <div>
-        <div>Name: </div>
-        {/* age gives object: "count":576,"name":"sidra","age":43  so age.age*/}
+        <div>Name: {params.name}</div>
         <div>Age: {age?.age}</div>
         <div>Gender: {gender?.gender}</div>
         <div>Country: {country?.country[0]?.country_id}</div>
       </div>
     </div>
   );
-}
+};
+
+export default Page;
