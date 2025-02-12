e22cd385d5fa6105dad05bd7817d5486ab67f275
SidraKayenat <sidrakayenatofficial@gmail.com>
2025-02-12 17:05:00 +0500

    initial changes

---
+++ src/app/predictor/[name]/page.tsx
@@ -0,0 +1,43 @@
+//CREATE 3 FUNCTIONS
+
+const getPredictedAge = async (name: string) => {
+  const res = await fetch(`https://api.agify.io/?name=${name}`);
+  return res.json();
+};
+const getPredictedGender = async (name: string) => {
+  const res = await fetch(`https://api.genderize.io/?name=${name}`);
+  return res.json();
+};
+const getPredictedCountry = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io/?name=${name}`);
+  return res.json();
+};
+
+interface Params {
+  params: { name: string };
+}
+
+export default async function Page({ params }: Params) {
+  const ageData = getPredictedAge(params.name);
+  const genderData = getPredictedGender(params.name);
+  const countryData = getPredictedCountry(params.name);
+
+  //if we have multiple APIs in same component, destrcuture it using array
+  //more than one promise at once
+  const [age, gender, country] = await Promise.all([
+    ageData,
+    genderData,
+    countryData,
+  ]);
+  return (
+    <div>
+      <div>
+        <div>Name: </div>
+        {/* age gives object: "count":576,"name":"sidra","age":43  so age.age*/}
+        <div>Age: {age?.age}</div>
+        <div>Gender: {gender?.gender}</div>
+        <div>Country: {country?.country[0]?.country_id}</div>
+      </div>
+    </div>
+  );
+}
